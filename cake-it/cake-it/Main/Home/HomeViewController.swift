a06fab565f8b5703865673489ea6425562c91d4e
corykim0829 <corykim0829@gmail.com>
2021-01-23 23:03:13 +0900

    feat: 프로젝트 생성 및 디렉토리 구분
    - Main, Order 스토리보드 생성
    
    #3

---
+++ cake-it/cake-it/Main/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  cake-it
+//
+//  Created by Cory Kim on 2021/01/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
