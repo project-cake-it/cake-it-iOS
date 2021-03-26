97363900de0d38158ab1569c888f75e7ba0dccd8
Cory <corykim0829@gmail.com>
2021-03-26 23:43:58 +0900

    feat: UIView extension 두개 추가
    - 오토레이아웃
    - corner radius
    
    #18

---
+++ cake-it/cake-it/Extensions/UIView/UIView+AutoLayout.swift
@@ -0,0 +1,47 @@
+//
+//  UIView+AutoLayout.swift
+//  cake-it
+//
+//  Created by Cory on 2021/03/26.
+//
+
+import UIKit
+
+extension UIView {
+  /// superView에 top, leading, trailing, bottom anchor를 동일하게 하여 꽉 채우게 하는 함수입니다.
+  func fillSuperView() {
+    guard let superView = self.superview else { return }
+    self.translatesAutoresizingMaskIntoConstraints = false
+    self.topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+    self.leadingAnchor.constraint(equalTo: superView.leadingAnchor).isActive = true
+    self.trailingAnchor.constraint(equalTo: superView.trailingAnchor).isActive = true
+    self.bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+  }
+  
+  func constraints(topAnchor: NSLayoutYAxisAnchor?,
+                   leadingAnchor: NSLayoutXAxisAnchor?,
+                   bottomAnchor: NSLayoutYAxisAnchor?,
+                   trailingAnchor: NSLayoutXAxisAnchor?,
+                   padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
+    self.translatesAutoresizingMaskIntoConstraints = false
+    
+    if let topAnchor = topAnchor {
+      self.topAnchor.constraint(equalTo: topAnchor, constant: padding.top).isActive = true
+    }
+    if let leadingAnchor = leadingAnchor {
+      self.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding.left).isActive = true
+    }
+    if let bottomAnchor = bottomAnchor {
+      self.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -padding.bottom).isActive = true
+    }
+    if let trailingAnchor = trailingAnchor {
+      self.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding.right).isActive = true
+    }
+    if size.width != 0 {
+      self.widthAnchor.constraint(equalToConstant: size.width).isActive = true
+    }
+    if size.height != 0 {
+      self.heightAnchor.constraint(equalToConstant: size.height).isActive = true
+    }
+  }
+}
