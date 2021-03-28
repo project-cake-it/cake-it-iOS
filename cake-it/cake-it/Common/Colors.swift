0966aaa9d79cf4d9cf308b0848fabffe469a585a
Cory <corykim0829@gmail.com>
2021-03-28 23:11:36 +0900

    feat: Colors enum 구현

---
+++ cake-it/cake-it/Common/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  cake-it
+//
+//  Created by Cory on 2021/03/28.
+//
+
+import UIKit
+
+enum Colors {
+  static let white = UIColor(named: "white")!
+  static let grayscale01 = UIColor(named: "grayscale01")!
+  static let grayscale02 = UIColor(named: "grayscale02")!
+  static let grayscale03 = UIColor(named: "grayscale03")!
+  static let grayscale04 = UIColor(named: "grayscale04")!
+  static let grayscale05 = UIColor(named: "grayscale05")!
+  static let black = UIColor(named: "black")!
+}
