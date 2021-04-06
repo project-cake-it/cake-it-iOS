5c1a9aad49d67e31de26d6af6add86572ce29748
Cory <corykim0829@gmail.com>
2021-04-06 22:21:40 +0900

    feat: 디렉토리 정리
    #23

---
+++ cake-it/cake-it/Shops/Controllers/ShopsMainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ShopListMainViewController.swift
+//  cake-it
+//
+//  Created by Cory on 2021/02/16.
+//
+
+import UIKit
+
+final class ShopsMainViewController: BaseViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    configure()
+  }
+}
+
+extension ShopsMainViewController {
+  private func configure() {
+    
+  }
+}
