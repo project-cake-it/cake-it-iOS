9831554f6cf1259732d6aa302c2d2f119bc81bb5
theodore <bethejustice@gmail.com>
2021-05-06 01:29:08 +0900

    #33 feat: savedItems cake images page

---
+++ cake-it/cake-it/Saved/CakeListSubViewController+CollectionViewDataSource.swift
@@ -0,0 +1,36 @@
+//
+//  CakeListSubViewController+CollectionViewDataSource.swift
+//  cake-it
+//
+//  Created by theodore on 2021/05/05.
+//
+
+import UIKit
+
+extension CakeListSubViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+    return cakeImages.count;
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cakeImageCell =  collectionView.dequeueReusableCell(withReuseIdentifier: "CakeImageCell", for: indexPath) as! CakeImageCell
+    
+    let cakeImageURL = URL(string: cakeImages[indexPath.row])!
+    DispatchQueue.global().async {
+      let data = try? Data(contentsOf: cakeImageURL)
+      DispatchQueue.main.async {
+        cakeImageCell.cakeImageView.image = UIImage(data: data!)
+      }
+    }
+    
+    return cakeImageCell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    let width = collectionView.frame.width
+    let cellWidth = width/2
+    
+    return CGSize(width: cellWidth, height: cellWidth)
+  }
+}
--- cake-it/cake-it/Saved/CakeListSubViewController.swift
+++ cake-it/cake-it/Saved/CakeListSubViewController.swift
@@ -10,6 +10,38 @@ import XLPagerTabStrip
 
 final class CakeListSubViewController: UIViewController, IndicatorInfoProvider {
   
+  @IBOutlet weak var cakeImageCollectionView: UICollectionView!
+  
+  private(set) var cakeImages: [String] = []
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    fetchCakeImages()
+    configureCakeImageCollectionView()
+  }
+  
+  private func fetchCakeImages() {
+    //TODO: 서버통신
+    let tempImageURL = "https://postfiles.pstatic.net/MjAyMTAzMjVfMjUw/MDAxNjE2Njg0MTc2OTc5.uKjj9xmaLrbGIbhnwiF7qhOroinNd60gbl8Jr6rMH18g.R7eRAZeHfGBv-wb8VZwo-r9IRqSLS-8Phocr7oiQ-g8g.PNG.cory_kim/Screen_Shot_2021-03-25_at_11.51.45_PM.png?type=w966"
+    for _ in 0..<15 {
+      cakeImages.append(tempImageURL)
+    }
+    
+    cakeImageCollectionView.reloadData()
+  }
+  
+  private func configureCakeImageCollectionView() {
+    cakeImageCollectionView.delegate = self
+    cakeImageCollectionView.dataSource = self
+    
+    let layout = UICollectionViewFlowLayout()
+    layout.minimumLineSpacing = 0
+    layout.minimumInteritemSpacing = 0
+    cakeImageCollectionView.collectionViewLayout = layout
+  }
+  
+  //MARK: - XLPager
   func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
     return IndicatorInfo(title: Constants.CAKE_LIST_SUB_VIEW_TITLE)
   }
