1c25946a788e091c6291e6a3998db516ae9777ad
Cory <corykim0829@gmail.com>
2021-05-08 01:08:05 +0900

    feat: detailVC present
    #34

---
+++ cake-it/cake-it/Shops/Controllers/ShopDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ShopDetailViewController.swift
+//  cake-it
+//
+//  Created by Cory on 2021/05/08.
+//
+
+import UIKit
+
+final class ShopDetailViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
+
+// MARK: - Configuration
+
+extension ShopDetailViewController {
+  private func configure() {
+    
+  }
+}
