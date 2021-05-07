6f135c47a7e82e31acb7c797e4265cb719356903
Cory <corykim0829@gmail.com>
2021-05-08 01:53:10 +0900

    feat: 타이틀 부분 구현
    #34

--- cake-it/cake-it/Shops/Controllers/ShopDetailViewController.swift
+++ cake-it/cake-it/Shops/Controllers/ShopDetailViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 final class ShopDetailViewController: UIViewController {
   
+  @IBOutlet weak var shopNameLabel: UILabel!
+  @IBOutlet weak var addressLabel: UILabel!
+  @IBOutlet weak var savedCountLabel: UILabel!
+  @IBOutlet weak var showAvailableDateButton: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    configure()
+  }
+  
+  @IBAction func backButtonDidTap(_ sender: Any) {
+    dismiss(animated: false, completion: nil)
   }
 }
 
@@ -19,6 +29,12 @@ final class ShopDetailViewController: UIViewController {
 
 extension ShopDetailViewController {
   private func configure() {
-    
+    configureViews()
+  }
+  
+  private func configureViews() {
+    showAvailableDateButton.layer.borderWidth = 1.0
+    showAvailableDateButton.layer.borderColor = Colors.pointB.cgColor
+    showAvailableDateButton.round(cornerRadius: 4.0, clipsToBounds: true)
   }
 }
