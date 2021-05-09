c80f1081c4c1b86e68b50808696fc4583b93ec09
Cory <corykim0829@gmail.com>
2021-05-10 01:56:07 +0900

    feat: UIDevice extension 추가
    #34

---
+++ cake-it/cake-it/Extensions/UIDevice/UIDevice+hasNotch.swift
@@ -0,0 +1,15 @@
+//
+//  UIDevice+hasNotch.swift
+//  cake-it
+//
+//  Created by Cory on 2021/05/10.
+//
+
+import UIKit
+
+extension UIDevice {
+  var hasNotch: Bool {
+    let bottom = UIApplication.shared.keyWindow?.safeAreaInsets.bottom ?? 0
+    return bottom > 0
+  }
+}
