bbeee7d39c3c642c8a930d5f570e1fbf65ae5ba9
seungbong88 <seungbong8.8@gmail.com>
2021-05-19 14:04:05 +0900

    #32 - init 컨벤션 수정

--- cake-it/cake-it/Home/DesignDetail/ViewModel/DesignDetailViewModel.swift
+++ cake-it/cake-it/Home/DesignDetail/ViewModel/DesignDetailViewModel.swift
@@ -10,9 +10,15 @@ import Foundation
 class DesignDetailViewModel {
   var designDetailModel: DesignDetailModel?
   
-  init(storeName: String, storeAddress: String, isSaved: Bool,
-       designName: String, iamgeUrls: [String], themes: [String],
-       priceBySize: [String: String], kindOfCreams: [String], kindOfSheets: [String] ) {
+  init(storeName: String,
+       storeAddress: String,
+       isSaved: Bool,
+       designName: String,
+       iamgeUrls: [String],
+       themes: [String],
+       priceBySize: [String: String],
+       kindOfCreams: [String],
+       kindOfSheets: [String]) {
     
     designDetailModel = DesignDetailModel(storeName: storeName,
                                           storeAddress: storeAddress,
