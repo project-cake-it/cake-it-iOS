495adefb8bd19f8d588b1e6c401802765ce54248
seungbong88 <seungbong8.8@gmail.com>
2021-06-21 00:39:05 +0900

    #61 - 디자인 리스트 테마 필터 구현, QueryString 함수 수정

---
+++ cake-it/cake-it/Common/Filter/FilterDetail/ThemeDetail/ThemeDetailView+UITableViewDataSource.swift
@@ -0,0 +1,33 @@
+//
+//  ThemeDetailView+UITableViewDataSource.swift
+//  cake-it
+//
+//  Created by seungbong on 2021/06/20.
+//
+
+import UIKit
+
+extension ThemeDetailView: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return FilterCommon.FilterTheme.allCases.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    if indexPath.row == 0 {
+      resetData()
+    }
+    
+    let identifier = String(describing: ThemeCell.self)
+    if let cell = tableView.dequeueReusableCell(withIdentifier: identifier) as? ThemeCell {
+      let themeType = FilterCommon.FilterTheme.allCases[indexPath.row]
+      cell.themeLabel.text = themeType.title
+      if selectedTheme == themeType {
+        cell.themeLabel.textColor = Colors.pointB
+        cell.themeLabel.font = Fonts.spoqaHanSans(weight: .Bold, size: 14.0)
+      }
+      return cell
+    }
+    return UITableViewCell()
+  }
+  
+}
