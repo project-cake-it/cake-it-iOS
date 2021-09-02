8534fae2a644f1a2f18eb81c1c8d45784fa2147c
Dongjun Kim <corykim0829@gmail.com>
2021-09-03 01:50:04 +0900

    feat: color assets 추가
    #134

--- cake-it/cake-it/Common/Colors.swift
+++ cake-it/cake-it/Common/Colors.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 enum Colors {
+  
+  // MARK: - Primary Colors
   static let primaryColor = UIColor(named: "primaryColor")!
+  static let primaryColor01 = UIColor(named: "primaryColor01")!
+  static let primaryColor02 = UIColor(named: "primaryColor02")!
+  static let primaryColor03 = UIColor(named: "primaryColor03")!
+  static let primaryColor04 = UIColor(named: "primaryColor04")!
+  static let primaryColor05 = UIColor(named: "primaryColor05")!
+  
   static let white = UIColor(named: "white")!
   static let grayscale01 = UIColor(named: "grayscale01")!
   static let grayscale02 = UIColor(named: "grayscale02")!
