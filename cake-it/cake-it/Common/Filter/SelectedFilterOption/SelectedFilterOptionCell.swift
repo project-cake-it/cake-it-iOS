ffcb7c620d61867f7bc110a97dd62f0d245cde81
Dongjun Kim <corykim0829@gmail.com>
2021-10-02 15:52:05 +0900

    feat: cell 구현
    #150

--- cake-it/cake-it/Common/Filter/SelectedFilterOption/SelectedFilterOptionCell.swift
+++ cake-it/cake-it/Common/Filter/SelectedFilterOption/SelectedFilterOptionCell.swift
@@ -7,11 +7,40 @@
 
 import UIKit
 
-class SelectedFilterOptionCell: UICollectionViewCell {
+protocol SelectedFilterOptionCellDelegate: AnyObject {
+  func selectedFilterOptionCell(closeButtonDidTap fromCell: SelectedFilterOptionCell)
+}
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+final class SelectedFilterOptionCell: UICollectionViewCell {
+  
+  @IBOutlet var filterOptionContainerView: UIView!
+  @IBOutlet var filterOptionTitleLabel: UILabel!
+  
+  weak var delegate: SelectedFilterOptionCellDelegate?
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    configure()
+  }
+  
+  func update(with title: String?) {
+    filterOptionTitleLabel.text = title
+  }
+  
+  @IBAction func closeButtonDidTap(_ sender: Any) {
+    delegate?.selectedFilterOptionCell(closeButtonDidTap: self)
+  }
+}
 
+// MARK: - Configuration
+
+extension SelectedFilterOptionCell {
+  private func configure() {
+    configureViews()
+  }
+  
+  private func configureViews() {
+    filterOptionContainerView.round(cornerRadius: 7)
+  }
 }
