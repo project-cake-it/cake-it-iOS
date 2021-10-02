ce0c116e4f46fc1ba12a6542d4cc37e17cf20529
Dongjun Kim <corykim0829@gmail.com>
2021-10-02 18:27:35 +0900

    feat: selectedFilterOption title 반환 함수 구현
    #150

--- cake-it/cake-it/Common/Filter/SelectedFilterOption/SelectedFilterOption.swift
+++ cake-it/cake-it/Common/Filter/SelectedFilterOption/SelectedFilterOption.swift
@@ -8,5 +8,44 @@
 import Foundation
 
 struct SelectedFilterOption {
-  let title: String?
+  let key: String
+  let value: String
+  
+  func title() -> String? {
+    var title: String? = nil
+    switch key {
+    case "order":
+      FilterCommon.FilterSorting.allCases.forEach {
+        if value == $0.value {
+          title = $0.title
+        }
+      }
+    case "size":
+      FilterCommon.FilterSize.allCases.forEach {
+        if value == $0.value {
+          title = $0.title
+        }
+      }
+    case "color":
+      FilterCommon.FilterColor.allCases.forEach {
+        if value == $0.value {
+          title = $0.title
+        }
+      }
+    case "category":
+      FilterCommon.FilterCategory.allCases.forEach {
+        if value == $0.value {
+          title = $0.title
+        }
+      }
+    case "location":
+      return value
+    case "pickup":
+      guard let date = DateFormatter.CakeOrderAvailableDateFormatter.date(from: value) else { return nil}
+      title = "\(date.month)월 \(date.day)일"
+    default:
+      break
+    }
+    return title
+  }
 }
