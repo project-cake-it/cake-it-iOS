a8c477d608019cb4b096376be886d01394ce9d52
Dongjun Kim <corykim0829@gmail.com>
2021-10-02 14:31:22 +0900

    chore: 에셋 추가
    #150

---
+++ cake-it/cake-it/Common/Filter/SelectedFilterOption/SelectedFilterOptionCell.swift
@@ -0,0 +1,17 @@
+//
+//  SelectedFilterOptionCell.swift
+//  cake-it
+//
+//  Created by Cory on 2021/10/02.
+//
+
+import UIKit
+
+class SelectedFilterOptionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
