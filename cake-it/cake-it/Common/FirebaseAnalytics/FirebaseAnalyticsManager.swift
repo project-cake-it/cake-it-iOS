b2860af58d42eb01a458780a5f404aabaced6ce6
Dongjun Kim <corykim0829@gmail.com>
2021-11-01 20:57:22 +0900

    feat: FirebaseAnalyticsManager 싱글톤 클래스 구현
    #216

---
+++ cake-it/cake-it/Common/FirebaseAnalytics/FirebaseAnalyticsManager.swift
@@ -0,0 +1,59 @@
+//
+//  FirebaseAnalyticsManager.swift
+//  cake-it
+//
+//  Created by Cory on 2021/10/25.
+//
+
+import Foundation
+import Firebase
+
+final class FirebaseAnalyticsManager {
+  
+  static let shared = FirebaseAnalyticsManager()
+  
+  init() { }
+  
+  enum Event {
+    static let contactShop = "contact_shop"
+  }
+  
+  func logContactShopEventInShopDetail(shopID: String, shopName: String, shopAddress: String) {
+    let currentDate = Date()
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    let dateString = dateFormatter.string(from: currentDate)
+    
+    Analytics.logEvent(
+      Event.contactShop,
+      parameters: [
+        "timestamp": dateString,
+        "shop_id": shopID,
+        "shop_name": shopName,
+        "shop_address": shopAddress
+      ])
+  }
+
+  func logContactShopEventInCakeDesignDetail(
+    shopID: String,
+    shopName: String,
+    shopAddress: String,
+    designID: String,
+    designName: String) {
+    let currentDate = Date()
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    let dateString = dateFormatter.string(from: currentDate)
+    
+    Analytics.logEvent(
+      Event.contactShop,
+      parameters: [
+        "timestamp": dateString,
+        "shop_id": shopID,
+        "shop_name": shopName,
+        "shop_address": shopAddress,
+        "design_id": designID,
+        "design_name": designName
+      ])
+  }
+}
